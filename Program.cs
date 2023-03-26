ac86c75c58f7f2c51373cdb133c2bfcaf50913bb
phusallver <103794258+phusallver@users.noreply.github.com>
2023-03-26 22:20:13 +0700

    implementasi design by contract

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SAYAYOUTUBEVIDEO;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            SayaTubeVideo vid1 = new SayaTubeVideo(
+                "Tutorial Design by Contract - Muhammad Nagif");
+            Console.WriteLine("Memutar video sebanyak 123 kali...");
+            Console.WriteLine();
+            vid1.IncreasePlayCount(123);
+            vid1.PrintVideoDetails();
+            Console.WriteLine();
+
+            Console.WriteLine("Menonton video sampai nyaris limit int");
+            for (int i = 0; i <428; i++)
+            {
+                vid1.IncreasePlayCount(5000000);
+            }
+            vid1.PrintVideoDetails();
+            Console.WriteLine();
+
+            Console.WriteLine("Coba ngelewatin batas overflow...");
+            vid1.IncreasePlayCount(10000000);
+        }
+        catch (OverflowException) 
+        {
+            Console.WriteLine("Overflow...");
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAYAYOUTUBEVIDEO
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        Random rendem = new Random();
+
+        public SayaTubeVideo(string title)
+        {
+            Debug.Assert(title != null && title.Length <= 100, "Judul tidak sesuai syarat");
+            this.id = rendem.Next();
+            this.title = title;
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount(int playCount)
+        {
+            Debug.Assert(playCount <= 10000000, "Jumlah penayangan tidak sesuai syarat");
+            this.playCount = checked(this.playCount + playCount);
+        }
+        public void PrintVideoDetails()
+        {
+            System.Console.WriteLine("Judul video            : " + this.title);
+            System.Console.WriteLine("ID video               : " + this.id);
+            System.Console.WriteLine("Sudah diputar sebanyak : " + this.playCount);
+        }
+    }
+}
